549a26a31e60e8869333ea2e9564b4362fa57143
IkeVoodoo <55694349+IkeVoodoo@users.noreply.github.com>
2022-03-02 11:26:57 +0100

    Update ComplexCommand.java

--- examples/ComplexCommand.java
+++ examples/ComplexCommand.java
@@ -4,6 +4,7 @@ public class ComplexCommand {
     @SubCommand(path = "/give")
     @Argument(name = "player", type = Player.class)
     @Argument(name = "rank", type = String.class)
+    // The method here returns a string, however it can also return void, this is done to send a message to the sender
     private String give(Player player, String rank) {
         // Do something...
         return "&aDone!";
