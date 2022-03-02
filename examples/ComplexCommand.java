f0bf550ef0b0bb6e4598ec247ce9e3cae3e2a17c
IkeVoodoo <55694349+IkeVoodoo@users.noreply.github.com>
2022-03-02 13:33:43 +0100

    Update ComplexCommand.java

--- examples/ComplexCommand.java
+++ examples/ComplexCommand.java
@@ -5,18 +5,14 @@ public class ComplexCommand {
     // A path always starts with a / indicating root
     // Example would be /a/b/c where you need to run /rank a b c to access it
     @SubCommand("/give")
-    @Argument(name = "player", type = Player.class)
-    @Argument(name = "rank", type = String.class)
     // The method here returns a string, however it can also return void, this is done to send a message to the sender
-    private String give(Player player, String rank) {
+    private String give(@Argument("player") Player player, @Argument("rank") String rank) {
         // Do something...
         return "&aDone!";
     }
 
     @SubCommand("/take")
-    @Argument(name = "player", type = Player.class)
-    @Argument(name = "rank", type = String.class)
-    private String take(Player player, String rank) {
+    private String take(@Argument("player") Player player, @Argument("rank") String rank) {
         // Do something...
         return "&aDone!";
     }
